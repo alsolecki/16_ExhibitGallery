5cdca18c83784fa31170f7a26265a411b299514d
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-08-19 13:58:08 -0400

    added navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,25 +2,36 @@ import React from 'react'
 
 const Navbar = () => {
   
-      console.log(`hsl(${Math.random() * 360}, 100%, 75%)`)
+  const randomColor = `hsl(${Math.random() * 360}, 100%, 75%)`
 
   return (
     <>
-      <div className="nav-bar">
+      <div className="nav-bar"
+            style={{ backgroundColor: randomColor }}>
+        <div id="button" className="button-home" >
+            <a href="https://alsolecki.com/">
+            <img src="public/home-icon.svg" alt="Home Icon" width="20px" height="20px"/></a>
+        </div>
+        <div 
+          id="button"
+          className="button-project"
+          // style="color: red"
+          >
+            <a href="https://sofocoflier.netlify.app/"><h3>SoFoCo Dems</h3></a>
+        </div>
+        <div id="button" className="button-project">
+            <a href="https://exhibitgallery.netlify.app/"><h3>Exhibit Gallery</h3></a>
+        </div>
+        <div id="button" className="button-project">
+            <a href="https://foodclock.netlify.app/"><h3>FoodClock</h3></a>
+        </div>
         <div id="button" className="button-home">
-            <a href="https://www.alsolecki.com/"> <h4>Home</h4></a>
-        </div>
-        <div id="button" className="button-project">
-            <a href="https://www.alsolecki.com/sofocodems"><h3>SoFoCo Dems</h3></a>
-        </div>
-        <div id="button" className="button-project">
-            <a href="https://www.alsolecki.com/exhibits"><h3>Exhibit Gallery</h3></a>
-        </div>
-        <div id="button" className="button-project">
-            <a href="https://www.alsolecki.com/foodclock"><h3>FoodClock</h3></a>
+            <a href="https://babyouterspace.netlify.app/">
+            <img src="public/toy-duck-icon.svg" alt="Toy Duck Icon" width="20px" height="20px"/></a>
         </div>
         <div id="button" className="button-home">
-            <a href="https://babyouterspace.netlify.app/"><h4>Contact</h4></a>
+            <a href="https://alsolecki.com/">
+            <img src="public/male-icon.svg" alt="Male Icon" width="20px" height="20px"/></a>
         </div>
       </div>
     </>
